384999cead50f6ec0f6c0d80e360a7aab1c0657c
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-08-16 19:46:06 +0530

    feat: Patches
    Removes eslint uncessary config
    Adds .prettierrc
    Removes font awesome dependency
    Adds font awesome cdn
    Adds about
    Adds home container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { version, Component } from 'inferno';
+import { Component } from 'inferno';
 import SideNavigationBar from './components/side-nav-bar/SideNavigationBar';
+import Home from './components/home/Home';
+import About from './components/about/About';
 class App extends Component {
   render() {
     return (
       <div id="page-top">
-       <SideNavigationBar />
+        <SideNavigationBar />
+        <Home>
+          <About />
+          <hr class="m-0" />
+        </Home>
       </div>
     );
   }
