60e590b8499fcb1bee6353d13eca6576d86d631d
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-11-21 13:18:43 +0530

    Adds achievements section

---
+++ src/components/achievements/index.js
@@ -0,0 +1,14 @@
+const Achievements = () => {
+  return (
+    <section
+      class="resume-section p-3 p-lg-5 d-flex flex-column"
+      id="achievements"
+    >
+      <div class="my-auto">
+        <h2 class="mb-5">Achievements</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Achievements;
